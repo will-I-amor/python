f6dee81a25aedc7028f1e72ab106abbd97204aa7
will-I-amor <judy_apple@hotmail.com>
2017-07-09 19:12:37 -0500

    Update mycode.py

--- backtracking/L39CombinationSumI/mycode.py
+++ backtracking/L39CombinationSumI/mycode.py
@@ -4,8 +4,8 @@ def helper(nums,i,target,sol,G):
         G.append(sol)
     if target>0:
         for j in range(i,len(nums)):
-            if j>i and nums[j]==nums[j-1]:
-                continue
+            #if j>i and nums[j]==nums[j-1]:这2行，即使删了也不影响。因为不怕重复。
+            #    continue  之前写这个怕[1,2,3,3,3,3,4]怕有重复，但是有重复并不影响
             if nums[j]<=target:
                 #sol.append(nums[j])
                 helper(nums,j,target-nums[j],sol+[nums[j]],G)
